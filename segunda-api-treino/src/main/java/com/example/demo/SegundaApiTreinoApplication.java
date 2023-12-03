5db4e84d3b42ad34669931c3eb4cd73be465fd51
ericomurta1991 <erico_murta@yahoo.com.br>
2023-12-03 18:47:27 -0300

    first commit, added project base

---
+++ segunda-api-treino/src/main/java/com/example/demo/SegundaApiTreinoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundaApiTreinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundaApiTreinoApplication.class, args);
+	}
+
+}
